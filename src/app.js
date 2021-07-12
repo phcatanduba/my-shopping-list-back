4fb6978e92b0594218174844c5893b32597e1d1e
Pedro Henrique <phcatanduba@gmail.com>
2021-07-12 15:14:07 -0300

    post and get methods added

--- src/app.js
+++ src/app.js
@@ -1,10 +1,31 @@
-import express from "express";
-import cors from "cors";
+import express from 'express';
+import cors from 'cors';
 
-import connection from "./database.js";
+import connection from './database.js';
 
 const app = express();
 app.use(express.json());
 app.use(cors());
+
+app.get('/items', async (req, res) => {
+    try {
+        const request = await connection.query('SELECT * FROM items');
+        res.send(request.rows);
+    } catch (e) {
+        console.log(e);
+    }
+});
+
+app.post('/items', async (req, res) => {
+    try {
+        const request = await connection.query(
+            'INSERT INTO items (text) values ($1)',
+            [req.body.text]
+        );
+        res.sendStatus(201);
+    } catch (e) {
+        console.log(e);
+    }
+});
 
 export default app;
